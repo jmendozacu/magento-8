b61d24a08c0b1afab8c8d8bb725d465705cbc76f
Li Liu <Li.L.Liu@newegg.com>
2015-09-09 11:57:59 +0800

    admin backend controller

---
+++ app/code/local/Li/Customform/controllers/Adminhtml/Customform/IndexController.php
@@ -0,0 +1,9 @@
+<?php
+
+class Li_Customform_Adminhtml_Customform_IndexController extends Mage_Adminhtml_Controller_Action {
+
+    public function indexAction () {
+        echo '123';
+    }
+
+}
---
+++ app/code/local/Li/Customform/controllers/Adminhtml/Customform/SubscriptionController.php
@@ -0,0 +1,10 @@
+<?php
+
+class Li_Customform_Adminhtml_Customform_SubscriptionController extends Mage_Adminhtml_Controller_Action {
+
+    public function indexAction () {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+
+}
